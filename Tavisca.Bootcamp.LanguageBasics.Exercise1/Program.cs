46ba827b8b4a8d883df59b1b6ada6982dc953d6a
Rishabh <rjaiswal@tavisca.in>
2019-06-22 19:39:20 +0530

    Solved Assignment

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,28 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            int indexOfQMark = equation.IndexOf("?");
+            int indexOfMult = equation.IndexOf("*");
+            int indexOfEqual = equation.IndexOf("=");
+            int l = 0, cnt = 0, missDig = -1;
+            if (indexOfQMark == 0 || indexOfQMark == indexOfMult + 1 || indexOfQMark == indexOfEqual + 1)
+            {
+                l = 1;
+            }
+            for (; l < 10; l++)
+            {
+                string newEquation = equation.Replace("?", l + "");
+                int A = Convert.ToInt32(newEquation.Substring(0, indexOfMult));
+                int B = Convert.ToInt32(newEquation.Substring(indexOfMult + 1, indexOfEqual - indexOfMult - 1));
+                int C = Convert.ToInt32(newEquation.Substring(indexOfEqual + 1));
+                if (A * B == C)
+                {
+                    cnt++;
+                    missDig = l;
+                }
+            }
+            if (cnt != 1) { missDig = -1; }
+            return missDig;
         }
     }
 }
